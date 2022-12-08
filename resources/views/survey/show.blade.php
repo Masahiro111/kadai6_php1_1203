3053ace97e8085138261ca325adf3247d547645b
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-12-09 02:42:02 +0900

    fix

--- resources/views/survey/show.blade.php
+++ resources/views/survey/show.blade.php
@@ -17,7 +17,7 @@
 
                     <a
                        class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-                       href="/survey/take/{{ $survey->id . '-' . $survey->title }}">詳細</a>
+                       href="{{ env('APP_URL') }}/survey/take/{{ $survey->id . '-' . $survey->title }}">詳細</a>
                 </div>
             </div>
         </div>
